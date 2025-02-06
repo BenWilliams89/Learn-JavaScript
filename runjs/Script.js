3522acaafb2c903a5256168d3098bade00f2ebad
Ben <benwilliamscomapi@outlook.com>
2025-02-06 15:01:31 +0000

    Add conditional if statements

--- runjs/Script.js
+++ runjs/Script.js
@@ -2,3 +2,21 @@ console.log('Hello, World!');
 
 let total = 30 + 1
 console.log(total)
+
+// IF Statement //
+console.log('Before If Statement')
+let age = 19
+    if (age >= 18) {
+        console.log('You may enter the pub')
+    }
+console.log('After If Statement')
+
+// If statement with Math.random()
+
+console.log('Before Math Statement')
+let random = Math.random();
+if (random < 0.5) {
+    console.log('Your number is less then 0.5' )
+    console.log(random)
+}
+console.log('After Math Statement')

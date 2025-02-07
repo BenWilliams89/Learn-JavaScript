8a2f7604ce2df1c11b188d93a3a304c3eaddede1
Ben <benwilliamscomapi@outlook.com>
2025-02-07 13:54:34 +0000

    Add Switch Statements

--- runjs/Starter_Javascript.js
+++ runjs/Starter_Javascript.js
@@ -67,3 +67,29 @@ if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
     console.log("YOU GOT IT!!!");
 }
 
+
+// Switch Statements
+
+const day = 2;
+switch (day) {
+    case 1:
+        console.log('MONDAY');
+        break;
+    case 2:
+        console.log('TUESDAY');
+        break;
+    case 3:
+        console.log('WEDNESDAY');
+        break;
+    case 4:
+        console.log('THURSDAY');
+        break;
+    case 5:
+        console.log('FRIDAY');
+        break;
+    case 6: 
+        console.log('SATURDAY');
+            break;
+    default:
+        console.log('SUNDAY')
+}

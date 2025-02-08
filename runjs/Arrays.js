ad328f1080943005b83c6540741f0771845e36d8
Ben <benwilliamscomapi@outlook.com>
2025-02-08 12:54:20 +0000

    Add array examples & notes

--- runjs/Arrays.js
+++ runjs/Arrays.js
@@ -1,2 +1,9 @@
 // Arrays
 
+let students = [] // empty array
+
+let colors = ['red', 'orange', 'yellow'] // array of strings
+
+let lottoNums = [19, 22, 56, 12, 51] // array of numbers
+
+let stuff = [true, 68, 'cat', null] // a mixed array with boollean, number, string & null

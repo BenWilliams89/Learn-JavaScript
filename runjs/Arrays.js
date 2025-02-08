4941681955b91aa4b6773e9f12e22ed79c33a07a
Ben <benwilliamscomapi@outlook.com>
2025-02-08 13:11:29 +0000

    Add modifying an array & array file

--- runjs/Arrays.js
+++ runjs/Arrays.js
@@ -1,4 +1,4 @@
-// Arrays
+// Array examples
 
 let students = [] // empty array
 
@@ -7,3 +7,12 @@ let colors = ['red', 'orange', 'yellow'] // array of strings
 let lottoNums = [19, 22, 56, 12, 51] // array of numbers
 
 let stuff = [true, 68, 'cat', null] // a mixed array with boollean, number, string & null
+
+// Modifying an array:
+
+let colours = ['rad', 'orange', 'yallaw']
+
+colours[0] = 'red'
+console.log(colours)
+colours[2] = 'yellow'
+console.log(colours)

f63b69582c219eb87c51c6760c50ce9833be169f
Ben <benwilliamscomapi@outlook.com>
2025-02-09 11:30:49 +0000

    Add object example & notes file

---
+++ runjs/object.js
@@ -0,0 +1,10 @@
+// object data structure - key: value, - order doesn't matter
+
+const fitBitData = {
+    totalSteps: 30500,
+    totalMile: 15,
+    avgCalorieBurn: 200,
+    workoutsThisWeek: ' 5 of 7',
+    averageGoofSleep: '5:45'
+}
+

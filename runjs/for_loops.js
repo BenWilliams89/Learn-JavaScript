de157afc057c3e6021ee56a3bf7a57cb288fe4b4
Ben <benwilliamscomapi@outlook.com>
2025-02-12 10:27:49 +0000

    Add for loops

---
+++ runjs/for_loops.js
@@ -0,0 +1,58 @@
+// for loops have 3 stages:
+// the variable, the expression & the increment/decrement
+// let i = 1;      i <= 10;             i++
+
+
+// Instead of printing the same thing ten times 
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+
+// We use loops to save time
+
+// starts at 1 - stop at 10 - add 1 each time
+
+for (let i = 1; i <=10; i++) {
+    console.log(i)
+}
+
+// Another example
+
+for (let B = 1; B < 6; B++) {
+    console.log('Hello Ben')
+}
+
+// loop printing even numbers
+
+for (let E = 0; E <=20; E+= 2) {
+    console.log(E)
+}
+
+// count down from 100
+
+for (let h = 20; h >= 0; h--){
+    console.log(h);
+}
+
+// count down from 100 in 10s
+
+for (let t = 100; t >=0; t -= 10) {
+    console.log(t)
+}
+
+// multiple by 10
+
+for (let m = 10; m <= 1000; m *= 10) {
+    console.log(m);
+    
+}
+
+// 

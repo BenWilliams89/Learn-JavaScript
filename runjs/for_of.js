7868ec8a6bd38b6ded423ab74b20d65b7da174ca
Ben <benwilliamscomapi@outlook.com>
2025-02-13 16:47:54 +0000

    Add examples of for of loops

--- runjs/for_of.js
+++ runjs/for_of.js
@@ -2,3 +2,25 @@
 
 // For Of loops are a easy way to iterate through arrays
 
+const subreddit = ['cinge', 'books', 'chickens', 'funny', 'pics', 'football']
+
+// Normal for loop
+
+for (let i = 0; i < subreddit.length; i++) {
+    console.log(subreddit[i])
+}
+
+// this can be condesed with a for of loop
+
+const names = ['Ben', 'Emily', 'Sam', 'Clive', 'Elaine']
+
+for (name of names) {
+    console.log(name)
+}
+
+// iterating over strings
+
+for (let char of 'Help'){
+    console.log(char)
+}
+
--- runjs/nested_loops.js
+++ runjs/nested_loops.js
@@ -37,3 +37,5 @@
             console.log(row[d])
         }
     }
+
+    

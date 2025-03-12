98e5476b04d0328d444121fad766a3b04d740ea9
Ben <benwilliamscomapi@outlook.com>
2025-03-12 11:16:15 +0000

    Add first function exercise

--- runjs/functions.js
+++ runjs/functions.js
@@ -46,4 +46,21 @@ function greeting(name) {
 }
 greeting('Emily')
 
+// Exercise Define a function called rant which accepts a string argument called message.
+// The function should print out an uppercased version of message 3 times (with 3 separate calls to console.log).
 
+function rant(message) {
+    console.log(message.toUpperCase())
+    console.log(message.toUpperCase())
+    console.log(message.toUpperCase())
+}
+rant('i hate beets')
+
+// Alternative soloution to the above exercise
+
+function yell(mess) {
+    for(let i = 0; i < 3; i++){
+        console.log(mess.toUpperCase())
+    }
+}
+yell('i hate pears')

e0c65fa690396463d1f2f94844946983a20e27d6
Ben <benwilliamscomapi@outlook.com>
2025-03-12 10:32:05 +0000

    Add basic function example

--- runjs/functions.js
+++ runjs/functions.js
@@ -1,2 +1,30 @@
 // Functions
 
+// Saves duplication of code and make the code re-usable
+
+
+// function are a TWO step process
+
+// Step One = define the function
+
+function functionName() {
+    // do something
+}
+
+// example
+
+function grumpus() {
+    console.log('ugh...you again')
+    console.log('for the last time...')
+    console.log('LEAVE ME ALONE!!')
+}
+grumpus()
+
+// second example
+
+function singSong() {
+    console.log('DO')
+    console.log('RE')
+    console.log('ME')
+}
+singSong()

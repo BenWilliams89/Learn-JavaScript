9401ca095414d6bf2923ec5300aaae0b9d835f7d
Ben <benwilliamscomapi@outlook.com>
2025-03-13 09:12:16 +0000

    Add function Expressions

---
+++ runjs/function_expressions.js
@@ -0,0 +1,16 @@
+
+// Function Expressions = Storing a function in a variable
+
+
+const square = function(num) {
+    return num * num
+}
+console.log(square(7)) 
+
+
+let add = function(nums) {
+    return nums + nums
+}
+console.log(add(2,2))
+
+// functions are variables
